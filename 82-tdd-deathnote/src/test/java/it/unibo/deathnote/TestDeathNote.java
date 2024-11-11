9987ea6019b452b501c9b0cd97d56c1fb466ddf8
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-11 14:51:55 +0100

    Impelemented @Test testGetRule in class TestDeathNote

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,6 +1,12 @@
 package it.unibo.deathnote;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import it.unibo.deathnote.api.DeathNote;
 import it.unibo.deathnote.impl.DeathNoteImplementation;
@@ -9,9 +15,41 @@ class TestDeathNote {
 
     private DeathNote deathNote;
 
+    private static final int ACCEPTABLE_MESSAGE_LENGTH = 10;
+
     @BeforeEach
     void setUp() {
         this.deathNote = new DeathNoteImplementation();
+    }
+
+    @Test
+    void testGetRule() {
+        try {
+            this.deathNote.getRule(0);
+            Assertions.fail("Return the String in position 0 of deathNote.RULES was possible, but should have thrown an exception");
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e.getMessage());
+            assertFalse(e.getMessage().isBlank());
+            assertTrue(e.getMessage().length() >= ACCEPTABLE_MESSAGE_LENGTH);
+        }
+
+        try {
+            this.deathNote.getRule(-1);
+            Assertions.fail("Return the String in position -1 of deathNote.RULES was possible, but should have thrown an exception");
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e.getMessage());
+            assertFalse(e.getMessage().isBlank());
+            assertTrue(e.getMessage().length() >= ACCEPTABLE_MESSAGE_LENGTH);
+        }
+
+        try {
+            this.deathNote.getRule(this.deathNote.RULES.size() + 1);
+            Assertions.fail("Return the String in position RULES.size() + 1 of deathNote.RULES was possible, but should have thrown an exception");
+        } catch (IllegalArgumentException e) {
+            assertNotNull(e.getMessage());
+            assertFalse(e.getMessage().isBlank());
+            assertTrue(e.getMessage().length() >= ACCEPTABLE_MESSAGE_LENGTH);
+        }
     }
 
 }
