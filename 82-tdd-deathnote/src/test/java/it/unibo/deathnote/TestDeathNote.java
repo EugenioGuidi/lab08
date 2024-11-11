33529100e5ca60ecc97ed719ef99e0d2d56c8a06
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-11 14:25:20 +0100

    Implemented @BeforeEach method setUp in class TestDeathNote

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,17 @@
 package it.unibo.deathnote;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImplementation;
+
 class TestDeathNote {
+
+    private DeathNote deathNote;
+
+    @BeforeEach
+    void setUp() {
+        this.deathNote = new DeathNoteImplementation();
+    }
 
 }
