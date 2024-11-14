153c7173f5351b694569f32338442266ad3198fe
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-14 17:12:06 +0100

    Implemented @Test testDetailsOfDeath in class TestDeathNote

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -21,6 +21,9 @@ class TestDeathNote {
     private static final String SECOND_HUMAN = "Andrea Bianchi";
     private static final String DEFAULT_CAUSE_OF_DEATH = "heart attack";
     private static final String CAUSE_OF_DEATH = "Karting accident";
+    private static final String DETAILS_OF_DEATH = "ran for too long";
+    private static final int INVALID_CAUSE_TIME = 100;
+    private static final int INVALID_DETAILS_TIME = 6000 + INVALID_CAUSE_TIME;
 
     @BeforeEach
     void setUp() {
@@ -32,7 +35,7 @@ class TestDeathNote {
         assertNotNull(this.deathNote);
         try {
             this.deathNote.getRule(0);
-            Assertions.fail("Return the String in position 0 or less of deathNote.RULES was possible, but should have thrown an exception");
+            Assertions.fail("Returned the String in position 0 or less of deathNote.RULES was possible, but should have thrown an exception");
         } catch (IllegalArgumentException e) {
             assertNotNull(e.getMessage());
             assertFalse(e.getMessage().isBlank());
@@ -41,7 +44,7 @@ class TestDeathNote {
 
         try {
             this.deathNote.getRule(this.deathNote.RULES.size() + 1);
-            Assertions.fail("Return the String in position RULES.size() + 1 of deathNote.RULES was possible, but should have thrown an exception");
+            Assertions.fail("Returned the String in position RULES.size() + 1 of deathNote.RULES was possible, but should have thrown an exception");
         } catch (IllegalArgumentException e) {
             assertNotNull(e.getMessage());
             assertFalse(e.getMessage().isBlank());
@@ -71,7 +74,7 @@ class TestDeathNote {
     void testCauseOfDeath() {
         try {
             this.deathNote.writeDeathCause(CAUSE_OF_DEATH);
-            Assertions.fail("He wrote the causes of death even if the name of the person is not there, but should have thrown an exception");
+            Assertions.fail("Wrote the causes of death even if the name of the person is not there, but should have thrown an exception");
         } catch (IllegalStateException e) {
             assertNotNull(e.getMessage());
             assertFalse(e.getMessage().isBlank());
@@ -87,7 +90,7 @@ class TestDeathNote {
         assertEquals(CAUSE_OF_DEATH, this.deathNote.getDeathCause(SECOND_HUMAN));
 
         try {
-            Thread.sleep(100);
+            Thread.sleep(INVALID_CAUSE_TIME);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
@@ -97,4 +100,26 @@ class TestDeathNote {
         assertEquals(this.deathNote.getDeathCause(SECOND_HUMAN), CAUSE_OF_DEATH);
     }
 
+    @Test
+    void testDetailsOfDeath() {
+        try {
+            this.deathNote.writeDetails(CAUSE_OF_DEATH);
+            Assertions.fail("Wrote the details of death even if the name of the person is not there, but should have thrown an exception");
+        } catch (IllegalStateException e) {
+            assertNotNull(e.getMessage());
+            assertFalse(e.getMessage().isBlank());
+            assertTrue(e.getMessage().length() >= ACCEPTABLE_MESSAGE_LENGTH);
+        }
+        this.deathNote.writeName(FIRST_HUMAN);
+        assertEquals("", this.deathNote.getDeathDetails(FIRST_HUMAN));
+        assertTrue(this.deathNote.writeDetails(DETAILS_OF_DEATH));
+        assertEquals(DETAILS_OF_DEATH, this.deathNote.getDeathDetails(CAUSE_OF_DEATH));
+        this.deathNote.writeName(SECOND_HUMAN);
+        try {
+            Thread.sleep(INVALID_DETAILS_TIME);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        assertFalse(this.deathNote.writeDetails(DETAILS_OF_DEATH));
+    }
 }
